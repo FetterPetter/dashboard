e3cea8aef6d3f2ebe8984ef19c999b5c7cf1e3dc
FetterPetter <petterskjorten@gmail.com>
2025-12-09 15:51:26 +0100

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,4 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Application } from "./app/application.js";
 
-
 createRoot(document.getElementById("root")!).render(<Application />);
